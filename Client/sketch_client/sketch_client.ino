cd78d30dabd9df94aa43d35cf1d2f097a6d66244
Yohann <yohannassouline@hotmail.fr>
2020-03-18 15:44:37 +0100

    update client

--- Client/sketch_client/sketch_client.ino
+++ Client/sketch_client/sketch_client.ino
@@ -1,16 +1,26 @@
-int button = 16;
+/*
+** Assouline Yohann, 2020
+** WSFF
+** sketch_client
+*/
+
+const int BUTTON_PIN = 12;
+const int LED_PIN = 14;
 
 void setup()
 {
-	pinMode(button, INPUT);
-	Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+
+    Serial.begin(9600);
+    Serial.println("Booting !");
 }
 
 void loop()
 {
-	int button_state = digitalRead(button);
-
-	if (button_state) {
-		Serial.println("PRESSED !");
-	}
+    int button_state = digitalRead(BUTTON_PIN);
+    if (button_state) {
+        digitalWrite(LED_PIN, HIGH);
+    }
+    digitalWrite(LED_PIN, LOW);
 }
