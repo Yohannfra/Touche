69e052d9b3324474e53ffd966b4cb79272bc695b
Yohann <yohannassouline@hotmail.fr>
2020-03-23 14:12:13 +0100

    add doc and a sketch to print mac address

---
+++ get_esp32_mac_address/get_esp32_mac_address.ino
@@ -0,0 +1,15 @@
+// Complete Instructions to Get and Change ESP MAC Address:
+// https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+
+#include "WiFi.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.mode(WIFI_MODE_STA);
+    Serial.println(WiFi.macAddress());
+}
+
+void loop()
+{
+}
