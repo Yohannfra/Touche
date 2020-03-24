83682c11caf284196ab17f417eeeb2cb13eca3fd
Yohann <yohannassouline@hotmail.fr>
2020-03-24 11:50:03 +0100

    change build system for the get_mac address too, move doc pictures to the doc folder and updates the platformio files

--- get_esp32_mac_address/src/main.cpp
+++ get_esp32_mac_address/src/main.cpp
@@ -1,6 +1,7 @@
 // Complete Instructions to Get and Change ESP MAC Address:
 // https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
 
+#include <Arduino.h>
 #include "WiFi.h"
 
 void setup()
