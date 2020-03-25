db6a31eb2f9e50b560d840199b2d1774f6aa039c
Yohann <yohannassouline@hotmail.fr>
2020-03-25 20:38:18 +0100

    clean up shared constants files

--- shared/constants.cpp
+++ shared/constants.cpp
@@ -24,12 +24,3 @@ const uint8_t MAC_ADDR_LIST[][3][6] {
     },
     // ...
 };
-
-// ESP32 MAC ADDRESS
-/* const uint8_t MAC_ADDR[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; */
-
- // ESP32 SERVER MAC ADDRESS
-/* const uint8_t SERVER_MAC_ADDR[] = {0x24, 0x62, 0xAB, 0xB0, 0x73, 0x50}; */
-
-// How long the leds turn on when a player hit (ms)
-const unsigned short FENCING_BLINKING_TIME = 1000;
--- shared/constants.hpp
+++ shared/constants.hpp
@@ -31,10 +31,10 @@ To access a specific board:
         MAC_ADDR_LIST[SERIAL_ID][CLIENT_2_MAC_ADDR_INDEX]
 */
 
-/* extern const uint8_t MAC_ADDR[]; */
-/* extern const uint8_t SERVER_MAC_ADDR[]; */
-
-extern const unsigned short FENCING_BLINKING_TIME;
+// How long the leds turn on when a player hit (ms)
+#define FENCING_BLINKING_TIME 1000
+// How long between two hits for it to be a double hit (ms)
+#define FENCING_LAPS_DOUBLE_TOUCH 250
 
 typedef enum {
     ERROR,
