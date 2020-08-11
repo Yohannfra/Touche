6b7d8c5e90f1f2828bef8dd3efc8a5f6ee714e3d
Yohann <yohannassouline@hotmail.fr>
2020-08-11 07:32:12 +0200

    move server mac address to a seperate file

--- client/include/client.h
+++ client/include/client.h
@@ -1,9 +1,7 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include <stdint.h>
-
-static const uint8_t SERVER_MAC_ADDR[6] = {0x24, 0x62, 0xAB, 0xB0, 0x73, 0x50};
+#include "server_mac_addr.h"
 
 #define LED_GPIO (17)
 #define LED_GPIO_BM (1ULL << LED_GPIO)
---
+++ client/include/server_mac_addr.h
@@ -0,0 +1,8 @@
+#ifndef SERVER_MAC_ADDR_H
+#define SERVER_MAC_ADDR_H
+
+#include <stdint.h>
+
+static const uint8_t SERVER_MAC_ADDR[6] = {0x24, 0x62, 0xAB, 0xB0, 0x73, 0x50};
+
+#endif // SERVER_MAC_ADDR_H
