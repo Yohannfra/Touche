190ed609b2d80d9cdac3b2ff91390c49ee2a6865
Yohann <yohannassouline@hotmail.fr>
2020-12-01 12:18:46 +0100

    add debug log lib

---
+++ arduino_nano_shared/libraries/debug_log/include/DebugLog.hpp
@@ -0,0 +1,33 @@
+#ifndef DEBUGLOG_HPP
+#define DEBUGLOG_HPP
+
+#if DEBUG
+#    define DEBUG_LOG(msg)                                                    \
+        do {                                                                  \
+            Serial.print(msg);                                                \
+        } while (0);
+
+#    define DEBUG_LOG_LN(msg)                                                 \
+        do {                                                                  \
+            Serial.println(msg);                                              \
+        } while (0);
+
+#    define DEBUG_LOG_VAL(msg, val)                                           \
+        do {                                                                  \
+            Serial.print(msg);                                                \
+            Serial.println(val);                                              \
+        } while (0);
+#else
+#    define DEBUG_LOG(msg)                                                    \
+        do {                                                                  \
+        } while (0);
+
+#    define DEBUG_LOG_LN(msg)                                                 \
+        do {                                                                  \
+        } while (0);
+#    define DEBUG_LOG_VAL(msg, val)                                                 \
+        do {                                                                  \
+        } while (0);
+#endif
+
+#endif /* DEBUGLOG_HPP */
