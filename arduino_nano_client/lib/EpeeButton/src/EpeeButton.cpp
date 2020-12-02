5b20001f6b1af00730d1d47d3cbd6d4ad505e680
Yohann <yohannassouline@hotmail.fr>
2020-12-02 01:06:41 +0100

    rename button to epeebutton

---
+++ arduino_nano_client/lib/EpeeButton/src/EpeeButton.cpp
@@ -0,0 +1,6 @@
+#include "EpeeButton.hpp"
+
+EpeeButton::EpeeButton()
+{
+}
+
---
+++ arduino_nano_client/lib/EpeeButton/src/EpeeButton.hpp
@@ -0,0 +1,12 @@
+#ifndef EPEEBUTTON_HPP
+#define EPEEBUTTON_HPP
+
+class EpeeButton
+{
+private:
+    /* data */
+public:
+    EpeeButton(/* args */);
+};
+
+#endif /* EPEEBUTTON_HPP */
