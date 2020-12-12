e87ad2ab6fd22aeb0765df05d66455141e157cd6
Yohann <yohannassouline@hotmail.fr>
2020-12-12 17:18:35 +0100

    update button to use INPUT_PULLUP

--- arduino_nano_client/lib/EpeeButton/src/EpeeButton.cpp
+++ arduino_nano_client/lib/EpeeButton/src/EpeeButton.cpp
@@ -5,14 +5,16 @@
     Using this method to make it faster
 */
 
-#define BUTTON_PIN (1 << 3) // PD3
+#define BUTTON_PIN (3)
+#define BUTTON_PIN_BM (1 << BUTTON_PIN) // PD3
 
 EpeeButton::EpeeButton()
 {
-    DDRD &= ~BUTTON_PIN; // set pin 3 as input
+    DDRD &= ~BUTTON_PIN_BM; // set pin 3 as input
+    PORTD |= BUTTON_PIN_BM; // enable INPUT_PULLUP on pin 3
 }
 
 bool EpeeButton::isPressed() const
 {
-    return PIND & BUTTON_PIN; // read pin 3
+    return !(PIND & BUTTON_PIN_BM); // is inversed becaused of INPUT_PULLUP
 }
