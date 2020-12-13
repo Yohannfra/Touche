43a6f7ee37a78eee30201ca6637452dc0311abe0
Yohann <yohannassouline@hotmail.fr>
2020-12-13 13:22:44 +0100

    remove useless constants

--- arduino_nano_shared/libraries/constants/include/fencingConstants.h
+++ arduino_nano_shared/libraries/constants/include/fencingConstants.h
@@ -7,8 +7,4 @@
 // How long between two hits for it to be a double hit (ms)
 #define FENCING_LAPS_DOUBLE_TOUCH 250 // 0.25 secs
 
-#define FENCING_LAPS_GROUND_NO_TOUCH 200 * 1000 // 200 ms, too much i think but to test
-
-/* #define TIME_BEFORE_SHOWING_HIT 150000 // 150 ms */
-
 #endif // FENCINGCONSTANTS_H
