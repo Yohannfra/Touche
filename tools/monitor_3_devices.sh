e01879f150d6453c8ba233c685c297ce18abe0c5
Yohann <yohannassouline@hotmail.fr>
2020-12-13 13:20:27 +0100

    add a tool to open 3 screens at the same time

---
+++ tools/monitor_3_devices.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PORT1="/dev/ttyUSB0"
+PORT2="/dev/ttyUSB1"
+PORT3="/dev/ttyUSB2"
+
+BAUDRATE="9600"
+
+tmux rename-window -t 0 'Main'
+
+tmux split-window -h
+tmux split-window -h
+
+tmux select-layout even-horizontal
+
+tmux send-keys -t 'Main' "screen ${PORT1} ${BAUDRATE}" C-m
+
+tmux selectp -L
+tmux send-keys -t 'Main' "screen ${PORT2} ${BAUDRATE}" C-m
+
+tmux selectp -L
+tmux send-keys -t 'Main' "screen ${PORT3} ${BAUDRATE}" C-m
