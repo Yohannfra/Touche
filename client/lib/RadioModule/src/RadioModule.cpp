00bcba2cbab1102099c7a92eeea9587a91378ae2
Yohann <yohannassouline@hotmail.fr>
2021-01-28 23:10:20 +0100

    fix range problem by sending the packet in a loop

--- client/lib/RadioModule/src/RadioModule.cpp
+++ client/lib/RadioModule/src/RadioModule.cpp
@@ -23,7 +23,7 @@ void RadioModule::init()
     }
     _radio.openWritingPipe(pipe);
     _radio.openReadingPipe(1, pipe);
-    _radio.setPALevel(RF24_PA_MIN);
+    _radio.setPALevel(RF24_PA_MAX);
     _radio.setDataRate(RF24_250KBPS);
 }
 
@@ -40,11 +40,17 @@ void RadioModule::sendMsg(int8_t id, payload_type_e payload)
     utils::print_bin("packet", packet);
     #endif
 
-    bool ack = _radio.write(&packet, sizeof(packet));
-
-    if (ack) {
-        DEBUG_LOG_LN("ACK received");
-    } else {
-        DEBUG_LOG_LN("ACK not received");
-    }
+    uint8_t try_times = 0;
+    const uint8_t MAX_TRY = 10; // number of times trying to send befor timeout
+    do {
+        bool ack = _radio.write(&packet, sizeof(packet));
+        if (ack) {
+            DEBUG_LOG_LN("ACK received");
+            return;
+        } else {
+            DEBUG_LOG_LN("ACK not received");
+        }
+        try_times += 1;
+    } while (try_times < MAX_TRY);
+    DEBUG_LOG_LN("TIMEOUT: Couldn't send message");
 }
