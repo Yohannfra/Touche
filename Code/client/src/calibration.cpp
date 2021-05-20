e67e563b04cfb5d6a8579501dc063f60cbba4deb
Yohann <yohannassouline@hotmail.fr>
2021-05-20 02:15:07 +0200

    Create calibration.cpp

---
+++ Code/client/src/calibration.cpp
@@ -0,0 +1,49 @@
+/*
+Touché Project
+Copyright (C) 2021 Assouline Yohann
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ArduinoLog.h"
+#include "RadioModule.hpp"
+#include "VirtualGround.hpp"
+#include "protocol.h"
+#include "touche.h"
+#include "utils.hpp"
+
+#include <Arduino.h>
+
+extern VirtualGround virtualGround;
+extern RadioModule radio_module;
+
+void run_calibration_process()
+{
+    Log.notice("== Starting calibration ==");
+    radio_module.sendMsg(CALIBRATION_STARTING);
+
+    while (virtualGround.calibrate() == false) {
+        if (weapon.isHitting(current_weapon) == Weapon::NONE) {
+            Log.warning("Button released during calibration");
+            radio_module.sendMsg(CALIBRATION_FAILED);
+            virtualGround.end_calibration(false);
+            delay(500);
+            return;
+        }
+    }
+    Log.notice("== Calibration Done ==");
+    virtualGround.end_calibration(true);
+    radio_module.sendMsg(CALIBRATION_END);
+    delay(500);
+}
