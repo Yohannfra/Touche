585ca5e438912b44c578ab817ee9c0711d4bf3f0
Yohann Assouline <yohannassouline@hotmail.fr>
2022-02-23 02:10:47 +0100

    Fix led_ring driver functions

--- Code/server/src/led_ring/led_ring.c
+++ Code/server/src/led_ring/led_ring.c
@@ -14,13 +14,7 @@ LOG_MODULE_REGISTER(led_ring, LOG_LEVEL_INF);
 #define STRIP_NODE DT_ALIAS(led_strip)
 #define STRIP_NUM_PIXELS DT_PROP(DT_ALIAS(led_strip), chain_length)
 
-static const struct led_rgb colors[] = {
-    {0x0f, 0x00, 0x00}, /* red */
-    {0x00, 0x0f, 0x00}, /* green */
-    {0x00, 0x00, 0x0f}, /* blue */
-};
-
-struct led_rgb pixels[STRIP_NUM_PIXELS];
+static struct led_rgb pixels[STRIP_NUM_PIXELS];
 
 static const struct device *strip = DEVICE_DT_GET(STRIP_NODE);
 
@@ -32,12 +26,13 @@ int led_ring_init(void)
         LOG_ERR("LED strip device %s is not ready", strip->name);
         return EXIT_FAILURE;
     }
+    led_ring_turn_off();
     return EXIT_SUCCESS;
 }
 
 void led_ring_turn_off(void)
 {
-    memset(&pixels, 0x00, sizeof(pixels));
+    memset(pixels, 0x00, sizeof(pixels));
     int rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
 
     if (rc) {
@@ -47,13 +42,32 @@ void led_ring_turn_off(void)
 
 void led_ring_set_color(uint8_t r, uint8_t g, uint8_t b)
 {
-    size_t color = 0;
+    struct led_rgb color ={0, r, g, b};
 
-    memset(&pixels, 0x00, sizeof(pixels));
-    memcpy(&pixels[0], &colors[color], sizeof(struct led_rgb));
+    for (int i = 0; i < STRIP_NUM_PIXELS; i++) {
+        memcpy(&pixels[i], &color, sizeof(struct led_rgb));
+    }
+
     int rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
 
     if (rc) {
         LOG_ERR("couldn't update strip: %d", rc);
+    }
+}
+
+void led_ring_play_demo(void)
+{
+    while (1) {
+        LOG_INF("GREEN");
+        led_ring_set_color(0xff, 0, 0);
+        k_sleep(K_MSEC(500));
+
+        LOG_INF("RED");
+        led_ring_set_color(0, 0xff, 0);
+        k_sleep(K_MSEC(500));
+
+        LOG_INF("BLUE");
+        led_ring_set_color(0, 0, 0xff);
+        k_sleep(K_MSEC(500));
     }
 }
--- Code/server/src/led_ring/led_ring.h
+++ Code/server/src/led_ring/led_ring.h
@@ -3,10 +3,15 @@
 
 #include <stdint.h>
 
+#define RGB_COLOR_GREEN 0x00, 0xff, 0x00
+#define RGB_COLOR_RED 0xff, 0x00, 0x00
+
 int led_ring_init(void);
 
 void led_ring_turn_off(void);
 
 void led_ring_set_color(uint8_t r, uint8_t g, uint8_t b);
+
+void led_ring_play_demo(void);
 
 #endif
